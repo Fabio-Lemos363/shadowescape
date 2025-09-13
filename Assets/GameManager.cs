8703c3e89a16ce5614d10764847b359e8102e76e
Fabio-Lemos363 <fabiolemos363@gmail.com>
2025-09-13 13:53:35 -0300

    botão de jogar novamente
    primeira versão do botao de jogar novamente

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static GameManager instance;
+    public Button playAgain;
+
+
+    void Awake()
     {
-        
+        instance = this;
+        playAgain.onClick.AddListener(OnClick);
+
+    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClick()
     {
-        
+        Debug.Log("pressionado");
     }
 }
